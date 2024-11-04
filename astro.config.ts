9c5f337f7a07d0062942202b29ce2081330c904a
Melani <coraline2719@icloud.com>
2024-11-04 14:42:06 +0100

    Remove D2 (For now)

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import astroD2 from "astro-d2";
 
 export default defineConfig({
-  integrations: [mdx(), astroD2()],
+  integrations: [mdx()],
 });
