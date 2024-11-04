3484fcaa970daebe55c6421766b5ba8797fea464
Melani <coraline2719@icloud.com>
2024-11-04 16:25:00 +0100

    Unify layouts

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,3 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
-export default defineConfig({
-  integrations: [mdx()],
-});
+import m from "@astrojs/mdx";
+export default defineConfig({ integrations: [m()] });
